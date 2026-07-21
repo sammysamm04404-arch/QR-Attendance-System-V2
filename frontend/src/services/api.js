2bb8e43055a2dcee80b11f5f6bf0baa8a31b89f0
Samm <sammysamm04404@gmail.com>
2026-07-21 13:19:41 +0530

    Update Backend link for live server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://qr-attendance-system-v2.fastapicloud.dev"
 });
 
 api.interceptors.request.use(
